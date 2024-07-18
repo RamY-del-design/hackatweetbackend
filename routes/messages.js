1651424eb241210317c7bd9b9f22e2d06ed1a352
Xavier N <x.naidet@gmail.com>
2024-07-18 14:27:40 +0200

    synchro

--- routes/messages.js
+++ routes/messages.js
@@ -3,3 +3,13 @@ var router = express.Router();
 
 require('../models/connection');
 const User = require('../models/users');
+
+router.get('/', (req, res) => {
+    User.find().then(data => {
+      if (data) {
+        res.json({ result: true, canBookmark: data.canBookmark });
+      } else {
+        res.json({ result: false, error: 'User not found' });
+      }
+    });
+  });
